9128a6890524d52fa7a6c55913fa0b51b085f5f1
Divsiq <31387556+Kolya-kostevich@users.noreply.github.com>
2017-11-14 17:39:40 +0300

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,10 @@
+JQuery(document).ready(function () {
+
+
+    particlesJS.load('particles-js', 'js/js2.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+
+
+})
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
